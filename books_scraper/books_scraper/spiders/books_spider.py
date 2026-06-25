15e4ddd5b8401963b69a4532a1758f193e0cc845
NaimurRahmann <niamurreiman@gmail.com>
2026-06-25 15:06:49 +0600

    feat: collect all books per category

--- books_scraper/books_scraper/spiders/books_spider.py
+++ books_scraper/books_scraper/spiders/books_spider.py
@@ -2,6 +2,8 @@ import random
 
 import scrapy
 
+from books_scraper.items import BookItem
+
 
 class BooksSpider(scrapy.Spider):
     name = "books"
@@ -13,7 +15,7 @@ class BooksSpider(scrapy.Spider):
     async def start(self):
         base_url = self.settings.get("BOOKS_BASE_URL")
         yield scrapy.Request(url=base_url, callback=self.parse)
-        
+
     def parse(self, response):
         category_links = response.css("div.side_categories ul li ul li a")
 
@@ -37,8 +39,50 @@ class BooksSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=category["url"],
                 callback=self.parse_category,
-                cb_kwargs={"category": category["name"]},
+                cb_kwargs={"category": category["name"], "book_urls": []},
             )
 
-    def parse_category(self, response, category):
-        self.logger.info("Reached category page: %s", category)
+    def parse_category(self, response, category, book_urls):
+        for href in response.css("article.product_pod h3 a::attr(href)").getall():
+            book_urls.append(response.urljoin(href))
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            yield response.follow(
+                next_page,
+                callback=self.parse_category,
+                cb_kwargs={"category": category, "book_urls": book_urls},
+            )
+            return
+
+        self.logger.info(
+            "Category '%s' has %d books total.", category, len(book_urls)
+        )
+
+        sample_size = min(self.BOOKS_PER_CATEGORY, len(book_urls))
+        selected_books = random.sample(book_urls, sample_size)
+
+        self.logger.info(
+            "Selected %d books from '%s'.", len(selected_books), category
+        )
+
+        for url in selected_books:
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse_book,
+                cb_kwargs={"category": category},
+            )
+            
+    def parse_book(self, response, category):
+        item = BookItem()
+        item["title"] = response.css("div.product_main h1::text").get()
+        item["price"] = response.css("p.price_color::text").get()
+        item["availability"] = " ".join(
+            response.css("p.availability::text").getall()
+        ).strip()
+        item["product_url"] = response.url
+        item["image_url"] = response.urljoin(
+            response.css("div.item.active img::attr(src)").get()
+        )
+        item["category"] = category
+        yield item
